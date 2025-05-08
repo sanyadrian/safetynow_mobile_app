0d891847679e644e9b4745e2979d6300e928457e
Oleksandr Adrianov <oleksandradrianov@Oleksandrs-MacBook-Air-2.local>
2025-05-08 12:49:23 -0400

    Update backend: main.py and databases

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 # app/main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, talks, history, tickets
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register the auth router
 app.include_router(auth.router)
