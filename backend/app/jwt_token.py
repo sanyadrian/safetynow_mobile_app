fdcb76f05b72f3af10783661920dfd12fd256e0c
Oleksandr Adrianov <adrianovoleksandr@gmail.com>
2025-06-12 10:52:34 -0400

    Updated verify_token

--- backend/app/jwt_token.py
+++ backend/app/jwt_token.py
@@ -1,18 +1,24 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from typing import Optional
 
-SECRET_KEY = "sZVLy5nokbiYV3ajh8JzTz7BUINxzsyJG7nK_bcJ1-8"
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-def create_access_token(data: dict):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_access_token(token: str):
+def verify_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
